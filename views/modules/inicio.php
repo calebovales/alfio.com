753dab1b2a42d6c4e66736e0e8fdcab97102a303
calebovales <39095076+calebovales@users.noreply.github.com>
2018-06-23 17:23:14 -0400

    modulo de inicio
    se mostrara lo que el usuario vera al logearse

---
+++ views/modules/inicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="views/css/bootstrap.css" />
+    
+</head>
+<body>
+<div class="container">
+  <h2>Card Image</h2>
+  <p>Image at the top (card-img-top):</p>
+  <div class="card" style="width:400px">
+    <img class="card-img-top" alt="Card image">
+    <div class="card-body">
+      <h4 class="card-title">John Doe</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+  <br> 
+ 
+</div>
+
+</body>
+</html>
